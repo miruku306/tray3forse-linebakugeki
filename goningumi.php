09ccfe774933000730b16678cd483d45a5e94a69
miruku306 <mi.ma.2x9.28@gmail.com>
2025-07-14 14:18:34 +0900

    Update goningumi.php

--- goningumi.php
+++ goningumi.php
@@ -15,7 +15,7 @@ session_start();
             <div class="header-logo">🧠 LINE爆撃くん</div>
             <nav class="nav-menu">
             <ul>
-              <li><a href="index.php">📝 新規会員登録/ログイン</a></li>
+              <li><a href="login.php">📝 新規会員登録/ログイン</a></li>
               <li><a href="Top.php">🏠 ホーム</a></li>
               <li><a href="">👥 グループ情報</a></li>
               <li><a href="mypage.php">👤 マイページ（ユーザー情報）</a> </li>
